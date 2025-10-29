f62e620f7a4b4760cc2154644be2fe430aa73b20
Daniel Suo <danielsuo@gmail.com>
2025-10-29 14:17:58 -0400

    Update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -62,6 +62,7 @@ require('lazy').setup {
   },
   'mrjones2014/smart-splits.nvim',
   'tpope/vim-surround',
+  'tpope/vim-sleuth',
   'rcarriga/nvim-notify',
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
