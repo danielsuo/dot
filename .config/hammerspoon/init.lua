5f0b3500643388cd61e2fa9e0677bf173c5558ae
Daniel Suo <danielsuo@gmail.com>
2026-01-06 14:35:10 -0500

    Update

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,90 +1,89 @@
-require('hs.ipc')
+require("hs.ipc")
 
-hs.loadSpoon('URLDispatcher')
+hs.loadSpoon("URLDispatcher")
 
 spoon.URLDispatcher:start()
 hs.window.animationDuration = 0
 
-
 local launch_app = function(app)
-  return function()
-    hs.application.launchOrFocus(app)
-  end
+	return function()
+		hs.application.launchOrFocus(app)
+	end
 end
 
 -- Window management
 local move_window_to_next_screen = function()
-  -- get the focused window
-  local win = hs.window.focusedWindow()
-  -- get the screen where the focused window is displayed, a.k.a. current screen
-  local screen = win:screen()
-  -- compute the unitRect of the focused window relative to the current screen
-  -- and move the window to the next screen setting the same unitRect
-  win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
+	-- get the focused window
+	local win = hs.window.focusedWindow()
+	-- get the screen where the focused window is displayed, a.k.a. current screen
+	local screen = win:screen()
+	-- compute the unitRect of the focused window relative to the current screen
+	-- and move the window to the next screen setting the same unitRect
+	win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
 end
 
 local move_window_to_prev_screen = function()
-  -- get the focused window
-  local win = hs.window.focusedWindow()
-  -- get the screen where the focused window is displayed, a.k.a. current screen
-  local screen = win:screen()
-  -- compute the unitRect of the focused window relative to the current screen
-  -- and move the window to the next screen setting the same unitRect
-  win:move(win:frame():toUnitRect(screen:frame()), screen:previous(), true, 0)
+	-- get the focused window
+	local win = hs.window.focusedWindow()
+	-- get the screen where the focused window is displayed, a.k.a. current screen
+	local screen = win:screen()
+	-- compute the unitRect of the focused window relative to the current screen
+	-- and move the window to the next screen setting the same unitRect
+	win:move(win:frame():toUnitRect(screen:frame()), screen:previous(), true, 0)
 end
 
 -- Web
 local dispatch_url = function(url)
-  return function()
-    spoon.URLDispatcher:dispatchURL('', '', '', 'https://' .. url, -1)
-  end
+	return function()
+		spoon.URLDispatcher:dispatchURL("", "", "", "https://" .. url, -1)
+	end
 end
 
 local GRID_SIZE = 4
 local HALF_GRID_SIZE = GRID_SIZE / 2
-hs.grid.setGrid(GRID_SIZE .. 'x' .. GRID_SIZE)
+hs.grid.setGrid(GRID_SIZE .. "x" .. GRID_SIZE)
 hs.grid.setMargins({ 0, 0 })
 hs.window.animationDuration = 0
 
-local screenPositions       = {}
-screenPositions.full        = { x = 0, y = 0, w = GRID_SIZE, h = GRID_SIZE }
-screenPositions.left        = { x = 0, y = 0, w = HALF_GRID_SIZE, h = GRID_SIZE }
-screenPositions.right       = { x = HALF_GRID_SIZE, y = 0, w = HALF_GRID_SIZE, h = GRID_SIZE }
-screenPositions.middle      = { x = GRID_SIZE / 3, y = 0, w = HALF_GRID_SIZE, h = GRID_SIZE }
+local screenPositions = {}
+screenPositions.full = { x = 0, y = 0, w = GRID_SIZE, h = GRID_SIZE }
+screenPositions.left = { x = 0, y = 0, w = HALF_GRID_SIZE, h = GRID_SIZE }
+screenPositions.right = { x = HALF_GRID_SIZE, y = 0, w = HALF_GRID_SIZE, h = GRID_SIZE }
+screenPositions.middle = { x = GRID_SIZE / 3, y = 0, w = HALF_GRID_SIZE, h = GRID_SIZE }
 
-local moveWindowToPosition  = function(cell, window)
-  if window == nil then
-    window = hs.window.focusedWindow()
-  end
-  if window then
-    local screen = window:screen()
-    hs.grid.set(window, cell, screen)
-  end
+local moveWindowToPosition = function(cell, window)
+	if window == nil then
+		window = hs.window.focusedWindow()
+	end
+	if window then
+		local screen = window:screen()
+		hs.grid.set(window, cell, screen)
+	end
 end
 
 -- Multimedia
-local change_volume         = function(diff)
-  return function()
-    local current = hs.audiodevice.defaultOutputDevice():volume()
-    local new = math.min(100, math.max(0, math.floor(current + diff)))
-    if new > 0 then
-      hs.audiodevice.defaultOutputDevice():setMuted(false)
-    end
-    hs.alert.closeAll(0.0)
-    hs.alert.show('Volume ' .. new .. '%', {}, 0.5)
-    hs.audiodevice.defaultOutputDevice():setVolume(new)
-  end
+local change_volume = function(diff)
+	return function()
+		local current = hs.audiodevice.defaultOutputDevice():volume()
+		local new = math.min(100, math.max(0, math.floor(current + diff)))
+		if new > 0 then
+			hs.audiodevice.defaultOutputDevice():setMuted(false)
+		end
+		hs.alert.closeAll(0.0)
+		hs.alert.show("Volume " .. new .. "%", {}, 0.5)
+		hs.audiodevice.defaultOutputDevice():setVolume(new)
+	end
 end
 
-local toggle_mute           = function()
-  local mute = not hs.audiodevice.defaultOutputDevice():muted()
-  hs.audiodevice.defaultOutputDevice():setMuted(mute)
-  hs.alert.closeAll(0.0)
-  if mute then
-    hs.alert.show('Muted')
-  else
-    hs.alert.show('Unmuted')
-  end
+local toggle_mute = function()
+	local mute = not hs.audiodevice.defaultOutputDevice():muted()
+	hs.audiodevice.defaultOutputDevice():setMuted(mute)
+	hs.alert.closeAll(0.0)
+	if mute then
+		hs.alert.show("Muted")
+	else
+		hs.alert.show("Unmuted")
+	end
 end
 
 -- HYPER
@@ -105,47 +104,63 @@ end
 -- Headspace, it'll block launching apps that are currently on the blocked
 -- lists via hs.settings.
 
-local hyper                 = hs.hotkey.modal.new({}, nil)
+local hyper = hs.hotkey.modal.new({}, nil)
 
-hyper.pressed               = function()
-  hyper.key_pressed_in_modal = false
-  hyper:enter()
+hyper.pressed = function()
+	hyper.key_pressed_in_modal = false
+	hyper:enter()
 end
 
-hyper.released              = function()
-  hyper:exit()
-  if not hyper.key_pressed_in_modal then
-    hs.eventtap.keyStroke({}, 'escape')
-  end
+hyper.released = function()
+	hyper:exit()
+	if not hyper.key_pressed_in_modal then
+		hs.eventtap.keyStroke({}, "escape")
+	end
 end
 
 -- Set the key you want to be HYPER to F19 in karabiner or keyboard
 -- Bind the Hyper key to the hammerspoon modal
-hs.hotkey.bind({}, 'F19', hyper.pressed, hyper.released)
+hs.hotkey.bind({}, "F19", hyper.pressed, hyper.released)
 
 hyper.allowed = function(app)
-  if app.tags then
-    if hs.settings.get('only') then
-      return hs.fnutils.some(hs.settings.get('only'), function(tag)
-        return hs.fnutils.contains(app.tags, tag)
-      end)
-    else
-      if hs.settings.get('never') then
-        return hs.fnutils.every(hs.settings.get('never'), function(tag)
-          return not hs.fnutils.contains(app.tags, tag)
-        end)
-      end
-    end
-  end
-  return true
+	if app.tags then
+		if hs.settings.get("only") then
+			return hs.fnutils.some(hs.settings.get("only"), function(tag)
+				return hs.fnutils.contains(app.tags, tag)
+			end)
+		else
+			if hs.settings.get("never") then
+				return hs.fnutils.every(hs.settings.get("never"), function(tag)
+					return not hs.fnutils.contains(app.tags, tag)
+				end)
+			end
+		end
+	end
+	return true
 end
 
 hyper.launch = function(app)
-  if hyper.allowed(app) then
-    hs.application.launchOrFocusByBundleID(app.bundleID)
-  else
-    hs.notify.show('Blocked ' .. app.bundleID, 'You have to switch headspaces', '')
-  end
+	if hyper.allowed(app) then
+		hs.application.launchOrFocusByBundleID(app.bundleID)
+	else
+		hs.notify.show("Blocked " .. app.bundleID, "You have to switch headspaces", "")
+	end
+end
+
+local focus_agent_manager = function()
+	hyper.key_pressed_in_modal = true
+	local app = hs.application.get("Jetski")
+
+	if app then
+		local win = app:allWindows()[1]
+		if win then
+			win:focus()
+		else
+			hs.alert.show("App found, but no window open")
+		end
+	else
+		hs.alert.show("Application 'Agent Manager' not running")
+	end
 end
 
 -- Expects a configuration table with an applications key that has the
@@ -159,122 +174,144 @@ end
 --   },
 -- }
 hyper.start = function(config_table)
-  -- Use the hyper key with the application config to use the `hyper_key`
-  for _, app in pairs(config_table.applications) do
-    -- Apps that I want to jump to
-    if app.hyper_key then
-      hyper:bind({}, app.hyper_key, function()
-        hyper.key_pressed_in_modal = true
-        hyper.launch(app);
-      end)
-    end
+	-- Use the hyper key with the application config to use the `hyper_key`
+	for _, app in pairs(config_table.applications) do
+		-- Apps that I want to jump to
+		if app.hyper_key then
+			hyper:bind({}, app.hyper_key, function()
+				hyper.key_pressed_in_modal = true
+				hyper.launch(app)
+			end)
+		end
 
-    -- I use hyper to power some shortcuts in different apps If the app is closed
-    -- and I press the shortcut, open the app and send the shortcut, otherwise
-    -- just send the shortcut.
-    if app.local_bindings then
-      for _, key in pairs(app.local_bindings) do
-        hyper:bind({}, key, nil, function()
-          hyper.key_pressed_in_modal = true
-          if hs.application.find(app.bundleID) then
-            hs.eventtap.keyStroke({ 'cmd', 'alt', 'shift', 'ctrl' }, key)
-          else
-            hyper.launch(app)
-            hs.timer.waitWhile(
-              function() return hs.application.find(app.bundleID) == nil end,
-              function()
-                hs.eventtap.keyStroke({ 'cmd', 'alt', 'shift', 'ctrl' }, key)
-              end)
-          end
-        end)
-      end
-    end
-  end
+		-- I use hyper to power some shortcuts in different apps If the app is closed
+		-- and I press the shortcut, open the app and send the shortcut, otherwise
+		-- just send the shortcut.
+		if app.local_bindings then
+			for _, key in pairs(app.local_bindings) do
+				hyper:bind({}, key, nil, function()
+					hyper.key_pressed_in_modal = true
+					if hs.application.find(app.bundleID) then
+						hs.eventtap.keyStroke({ "cmd", "alt", "shift", "ctrl" }, key)
+					else
+						hyper.launch(app)
+						hs.timer.waitWhile(function()
+							return hs.application.find(app.bundleID) == nil
+						end, function()
+							hs.eventtap.keyStroke({ "cmd", "alt", "shift", "ctrl" }, key)
+						end)
+					end
+				end)
+			end
+		end
+	end
 end
 
 local config = {}
 config.applications = {
-  ['Homerow'] = {
-    bundleID = 'com.superultra.Homerow',
-    local_bindings = { 'space' }
-  },
+	["Homerow"] = {
+		bundleID = "com.superultra.Homerow",
+		local_bindings = { "space" },
+	},
 }
 hyper.start(config)
 
-hyper:bind({}, 'A', launch_app('Activity Monitor'))
-hyper:bind({}, 'B', dispatch_url('b.corp.google.com/home'))
-hyper:bind({}, 'C', launch_app('Google Chrome'))
-hyper:bind({}, 'E', dispatch_url('goto2.corp.google.com/sknow'))
-hyper:bind({}, 'F', launch_app('Finder'))
-hyper:bind({}, 'G', dispatch_url('github.com/jax-ml/jax'))
-hyper:bind({ 'cmd' }, 'H', nil, launch_app('Google Chat'))
-hyper:bind({ 'alt' }, 'H', nil, launch_app('Messenger'))
-hyper:bind({}, 'I', launch_app('Messages'))
-hyper:bind({ 'cmd' }, 'J', nil, dispatch_url('goto2.corp.google.com/jaxplorations'))
-hyper:bind({ 'cmd' }, 'L', nil, dispatch_url('calendar.google.com/calendar/u/0'))
-hyper:bind({ 'alt' }, 'L', nil, dispatch_url('calendar.google.com/calendar/u/1'))
-hyper:bind({}, 'M', dispatch_url('mail.google.com'))
-hyper:bind({ 'cmd' }, 'M', nil, dispatch_url('mail.google.com/mail/u/1'))
-hyper:bind({}, 'Q', dispatch_url('critique.corp.google.com'))
-hyper:bind({}, 'R', launch_app('Reminders'))
-hyper:bind({}, 'S', launch_app('Settings'))
-hyper:bind({}, 'T', launch_app('WezTerm'))
-hyper:bind({}, 'U', hs.reload)
-hyper:bind({}, 'V', launch_app('Jetski'))
-hyper:bind({ 'cmd' }, 'V', nil, launch_app('Cider'))
-hyper:bind({}, 'Y', hs.toggleConsole)
-hyper.bind({}, 'Z', function()
-    local win = hs.window.find('Agent Manager')
+hyper:bind({}, "A", launch_app("Activity Monitor"))
+hyper:bind({}, "B", dispatch_url("b.corp.google.com/home"))
+hyper:bind({}, "C", launch_app("Google Chrome"))
+hyper:bind({}, "E", dispatch_url("goto2.corp.google.com/sknow"))
+hyper:bind({}, "F", launch_app("Finder"))
+hyper:bind({}, "G", dispatch_url("github.com/jax-ml/jax"))
+hyper:bind({ "cmd" }, "H", nil, launch_app("Google Chat"))
+hyper:bind({ "alt" }, "H", nil, launch_app("Messenger"))
+hyper:bind({}, "I", launch_app("Messages"))
+hyper:bind({ "cmd" }, "J", nil, dispatch_url("goto2.corp.google.com/jaxplorations"))
+hyper:bind({ "cmd" }, "L", nil, dispatch_url("calendar.google.com/calendar/u/0"))
+hyper:bind({ "alt" }, "L", nil, dispatch_url("calendar.google.com/calendar/u/1"))
+hyper:bind({}, "M", dispatch_url("mail.google.com"))
+hyper:bind({ "cmd" }, "M", nil, dispatch_url("mail.google.com/mail/u/1"))
+hyper:bind({}, "Q", dispatch_url("critique.corp.google.com"))
+hyper:bind({}, "R", launch_app("Reminders"))
+hyper:bind({}, "S", launch_app("Settings"))
+hyper:bind({}, "T", launch_app("WezTerm"))
+hyper:bind({}, "U", hs.reload)
+hyper:bind({}, "V", launch_app("Jetski"))
+hyper:bind({ "cmd" }, "V", nil, launch_app("Cider"))
+hyper:bind({}, "X", focus_agent_manager)
+hyper:bind({}, "Y", hs.toggleConsole)
+hyper:bind({}, "UP", change_volume(5))
+hyper:bind({}, "DOWN", change_volume(-5))
+hyper:bind({}, "LEFT", toggle_mute)
 
-    if win then
-        win:focus()
-    else
-        hs.alert.show('Agent Manager not found.')
-    end
+hyper:bind({}, "[", function()
+	moveWindowToPosition(screenPositions.left)
 end)
-hyper:bind({}, 'UP', change_volume(5))
-hyper:bind({}, 'DOWN', change_volume(-5))
-hyper:bind({}, 'LEFT', toggle_mute)
-
-hyper:bind({}, '[', function() moveWindowToPosition(screenPositions.left) end)
-hyper:bind({ 'cmd' }, '[', function() move_window_to_prev_screen() end)
-hyper:bind({}, ']', function() moveWindowToPosition(screenPositions.right) end)
-hyper:bind({ 'cmd' }, ']', function() move_window_to_next_screen() end)
-hyper:bind({}, '=', function() moveWindowToPosition(screenPositions.full) end)
-hyper:bind({}, '\\', function() moveWindowToPosition(screenPositions.middle) end)
+hyper:bind({ "cmd" }, "[", function()
+	move_window_to_prev_screen()
+end)
+hyper:bind({}, "]", function()
+	moveWindowToPosition(screenPositions.right)
+end)
+hyper:bind({ "cmd" }, "]", function()
+	move_window_to_next_screen()
+end)
+hyper:bind({}, "=", function()
+	moveWindowToPosition(screenPositions.full)
+end)
+hyper:bind({}, "\\", function()
+	moveWindowToPosition(screenPositions.middle)
+end)
 
 local readline_hotkeys = {}
-readline_hotkeys.forward_word = hs.hotkey.new({ 'alt' }, 'f', function() hs.eventtap.keyStroke({ 'alt' }, 'right') end)
-readline_hotkeys.forward_select_word = hs.hotkey.new({ 'alt', 'shift' }, 'f', function() hs.eventtap.keyStroke({ 'alt', 'shift' }, 'right') end)
-readline_hotkeys.back_word = hs.hotkey.new({ 'alt' }, 'b', function() hs.eventtap.keyStroke({ 'alt' }, 'left') end)
-readline_hotkeys.back_select_word = hs.hotkey.new({ 'alt', 'shift' }, 'b', function() hs.eventtap.keyStroke({ 'alt', 'shift' }, 'left') end)
-readline_hotkeys.down_line = hs.hotkey.new({ 'ctrl' }, 'down', function() hs.eventtap.keyStroke({ 'ctrl' }, 'n') end)
-readline_hotkeys.down_select_line =hs.hotkey.new({ 'ctrl', 'shift' }, 'down', function() hs.eventtap.keyStroke({ 'ctrl', 'shift' }, 'n') end)
-readline_hotkeys.up_line = hs.hotkey.new({ 'ctrl' }, 'up', function() hs.eventtap.keyStroke({ 'ctrl' }, 'p') end)
-readline_hotkeys.up_select_line = hs.hotkey.new({ 'ctrl', 'shift' }, 'up', function() hs.eventtap.keyStroke({ 'ctrl', 'shift' }, 'p') end)
+readline_hotkeys.forward_word = hs.hotkey.new({ "alt" }, "f", function()
+	hs.eventtap.keyStroke({ "alt" }, "right")
+end)
+readline_hotkeys.forward_select_word = hs.hotkey.new({ "alt", "shift" }, "f", function()
+	hs.eventtap.keyStroke({ "alt", "shift" }, "right")
+end)
+readline_hotkeys.back_word = hs.hotkey.new({ "alt" }, "b", function()
+	hs.eventtap.keyStroke({ "alt" }, "left")
+end)
+readline_hotkeys.back_select_word = hs.hotkey.new({ "alt", "shift" }, "b", function()
+	hs.eventtap.keyStroke({ "alt", "shift" }, "left")
+end)
+readline_hotkeys.down_line = hs.hotkey.new({ "ctrl" }, "down", function()
+	hs.eventtap.keyStroke({ "ctrl" }, "n")
+end)
+readline_hotkeys.down_select_line = hs.hotkey.new({ "ctrl", "shift" }, "down", function()
+	hs.eventtap.keyStroke({ "ctrl", "shift" }, "n")
+end)
+readline_hotkeys.up_line = hs.hotkey.new({ "ctrl" }, "up", function()
+	hs.eventtap.keyStroke({ "ctrl" }, "p")
+end)
+readline_hotkeys.up_select_line = hs.hotkey.new({ "ctrl", "shift" }, "up", function()
+	hs.eventtap.keyStroke({ "ctrl", "shift" }, "p")
+end)
 
 local function disable_readline_hotkeys()
-  for _, hotkey in pairs(readline_hotkeys) do
-    hotkey:disable()
-  end
+	for _, hotkey in pairs(readline_hotkeys) do
+		hotkey:disable()
+	end
 end
 
 local function enable_readline_hotkeys()
-  for _, hotkey in pairs(readline_hotkeys) do
-    hotkey:enable()
-  end
+	for _, hotkey in pairs(readline_hotkeys) do
+		hotkey:enable()
+	end
 end
 
 -- Create the application watcher
-hs.application.watcher.new(function(appName, eventType, appObject)
-  if eventType == hs.application.watcher.activated then
-    -- When a new app is activated
-    if appName == 'WezTerm' then
-      -- If it's WezTerm, disable your global hotkeys
-      disable_readline_hotkeys()
-    else
-      -- Otherwise, enable them
-      enable_readline_hotkeys()
-    end
-  end
-end):start()
+hs.application.watcher
+	.new(function(appName, eventType, appObject)
+		if eventType == hs.application.watcher.activated then
+			-- When a new app is activated
+			if appName == "WezTerm" then
+				-- If it's WezTerm, disable your global hotkeys
+				disable_readline_hotkeys()
+			else
+				-- Otherwise, enable them
+				enable_readline_hotkeys()
+			end
+		end
+	end)
+	:start()
